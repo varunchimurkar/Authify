e2f33b6e8e5458e7edad956a16e4ac7749328a53
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-08-05 16:17:41 +0530

    Modify verifyUser

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -111,19 +111,34 @@ const verifyUser = async (req, res) => {
     })
   }
 
-  const user = await User.findOne({
-    verificationToken: token
-  })
-
-  if (!user) {
-    return res.status(400).json({
-      message: "Invalid token"
+  try {
+    const user = await User.findOne({
+      verificationToken: token
     })
-  }
 
-  user.isVerified = true
-  user.verificationToken = undefined
-  await user.save()
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid token"
+      })
+    }
+
+    user.isVerified = true
+    user.verificationToken = undefined
+    await user.save()
+
+
+    res.status(200).json({
+      message: "User verified successfully",
+      success: true,
+    });
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "User not verified",
+      error,
+      success: false,
+    });
+  }
 }
 
 const login = async (req, res) => {
